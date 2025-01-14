fd45584ded9bfe98d4f11fff345c13c1ed52c56e
Sundeepk <155922078+sk0879@users.noreply.github.com>
2025-01-14 09:46:16 +0500

    Update evaluation.py

--- Development/evaluation.py
+++ Development/evaluation.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import classification_report
 
-def evaluate_model(model, x_test, y_test, target_names=['Not Fraud', 'Fraud']):
-    predictions = model.predict(x_test)
-    report = classification_report(y_test, predictions, target_names=target_names)
-    return report
+def evaluate_model(model, x_val, y_val, model_name):
+    predictions = model.predict(x_val)
+    report = classification_report(y_val, predictions, target_names=['Not Fraud', 'Fraud'])
+    print(f"Classification Report for {model_name}:\n{report}")
